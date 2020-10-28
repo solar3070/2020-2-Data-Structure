e81e35f54aa52fb3b9acb9c8cd88001457089231
unknown <solar3070@naver.com>
2020-10-28 23:35:34 +0900

    week9

---
+++ week9/HW4_2/HW4_2.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+typedef int element;
+typedef struct TreeNode {
+	element key;
+	struct TreeNode *left, *right;
+} TreeNode;
+
+// 순환적인 탐색 함수
+TreeNode * search(TreeNode * node, int key)
+{
+	if (node == NULL) return NULL;
+	if (key == node->key) return node;
+	else if (key < node->key)
+		return search(node->left, key);
+	else
+		return search(node->right, key);
+}
+
+TreeNode * new_node(int item)
+{
+	TreeNode * temp = (TreeNode *)malloc(sizeof(TreeNode));
+	temp->key = item;
+	temp->left = temp->right = NULL;
+	return temp;
+}
+
+TreeNode * insert_node(TreeNode * node, int key)
+{
+	if (node == NULL) return new_node(key);
+	if (key < node->key)
+		node->left = insert_node(node->left, key);
+	else if (key > node->key)
+		node->right = insert_node(node->right, key);
+	return node;
+}
+
+TreeNode * min_value_node(TreeNode * node)
+{
+	TreeNode * current = node;
+	while (current->left != NULL) // 맨 왼쪽 단말 노드를 찾으러 내려감
+		current = current->left;
+	return current;
+}
+
+TreeNode * delete_node(TreeNode * root, int key)
+{
+	TreeNode * temp;
+	if (root == NULL) return root;
+	if (key < root->key)
+		root->left = delete_node(root->left, key);
+	else if (key > root->key)
+		root->right = delete_node(root->right, key);
+	else {
+		if (root->left == NULL) {
+			TreeNode * temp = root->right;
+			free(root);
+			return temp;
+		}
+		else if (root->right == NULL) {
+			TreeNode * temp = root->left;
+			free(root);
+			return temp;
+		}
+		temp = min_value_node(root->right);
+		root->key = temp->key; 
+		root->right = delete_node(root->right, temp->key);
+	}
+	return root;
+}
+
+void preorder(TreeNode * root) {
+	if (root) {
+		printf("%d ", root->key);  
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+
+int get_height(TreeNode *root)
+{
+	if (root == NULL)
+		return 0;
+	else
+		return 1 + MAX(get_height(root->left), get_height(root->right));
+}
+
+int get_node_count(TreeNode *root)
+{
+	if (root == NULL)
+		return 0;
+	else
+		return 1 + get_node_count(root->left) + get_node_count(root->right);
+}
+
+int get_maximum(TreeNode *root) 
+{
+	if (root == NULL)
+		return 0;
+	while (root->right != NULL)
+		root = root->right;
+	return root->key;
+}
+
+int get_minimum(TreeNode *root) 
+{
+	if (root == NULL)
+		return 0;
+	while (root->left != NULL)
+		root = root->left;
+	return root->key;
+}
+
+int main(void)
+{
+	TreeNode *root = NULL;
+	char ch;
+	int key;
+
+	printf("enter i(nsert), d(elete), s(earch), p(rint), h(eight), c(ount), m(ax), n(min), q(uit): ");
+	scanf("%c", &ch);
+
+	while (ch != 'q') {
+		switch(ch) {
+		case 'i':
+			printf("삽입할 key값 입력: ");
+			scanf("%d", &key);
+			root = insert_node(root, key);
+			break;
+		case 'd':
+			printf("삭제할 key값 입력: ");
+			scanf("%d", &key);
+			root = delete_node(root, key);
+			break;
+		case 's':
+			printf("탐색할 key값 입력: ");
+			scanf("%d", &key);
+			if (search(root, key) != NULL)
+				printf("있음\n");
+			else
+				printf("없음\n");
+			break;
+		case 'p':
+			preorder(root);
+			printf("\n");
+			break;
+		case 'h':
+			printf("트리의 높이는 %d\n", get_height(root));
+			break;
+		case 'c':
+			printf("노드의 개수는 %d\n", get_node_count(root));
+			break;
+		case 'm':
+			printf("가장 큰 값은 %d\n", get_maximum(root));
+			break;
+		case 'n':
+			printf("가장 작은 값은 %d\n", get_minimum(root));
+			break;
+		}
+		while (getchar() != '\n');
+		printf("enter i(nsert), d(elete), s(earch), p(rint), h(eight), c(ount), m(ax), n(min), q(uit): ");
+		scanf("%c", &ch);
+	}
+}
